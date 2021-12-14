483ee69e5b5f7f6accbf4f11e2b091575d4862ab
Rishabh Shairy <rishusinha8@gmail.com>
2021-12-14 16:27:55 +0530

    Added tests for 13 digit isbn

--- tests/com/rishabhshairy/isbn/ValidateISBNTest.java
+++ tests/com/rishabhshairy/isbn/ValidateISBNTest.java
@@ -3,23 +3,58 @@ package com.rishabhshairy.isbn;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class ValidateISBNTest {
+public class ValidateISBNTest {
 
 	@Test
-	void checkAValidISBN() {
+	public void checkAValid10DigitsISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.validateISBN("9355201885");
-		assertTrue("Value one",result);
+		assertTrue("Value one", result);
+	}
+
+	@Test
+	public void tenDigitsISBNNumbersEndingInAnXAreValid() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.validateISBN("012000030X");
+		assertTrue("Value one", result);
+	}
+
+	@Test
+	public void checkAValid13DigitsISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.validateISBN("9788175992771");
+		assertTrue("Value one", result);
+	}
+
+	@Test
+	public void checkAnInvalid13Digits() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.validateISBN("9788175992770");
+		assertFalse("Value one", result);
 	}
 	
 	@Test
-	void checkAnInvalidISBN() {
+	public void checkAnInvalid10DigitsISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.validateISBN("9355201889");
 		assertFalse("Checking invalid isbn case", result);
-		
+
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nineDigitsNotAllowed() {
+		ValidateISBN validator = new ValidateISBN();
+		validator.validateISBN("935520188");
+		validator.validateISBN("helloworld");
+
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void nonNumbericISBNsNotAllowed() {
+		ValidateISBN validator = new ValidateISBN();
+		validator.validateISBN("helloworld");
 	}
 
 }
