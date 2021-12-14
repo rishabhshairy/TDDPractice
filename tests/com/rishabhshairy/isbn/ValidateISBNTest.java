da6f811ebb4c2431480272e51309fdb9681cb62d
Rishabh Shairy <rishusinha8@gmail.com>
2021-12-14 14:19:57 +0530

    Created Test file
    Writing tests to derive logic

--- tests/com/rishabhshairy/isbn/ValidateISBNTest.java
+++ tests/com/rishabhshairy/isbn/ValidateISBNTest.java
@@ -7,8 +7,17 @@ import org.junit.jupiter.api.Test;
 class ValidateISBNTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void checkAValidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.validateISBN(9355201);
+		assertTrue(result);
+	}
+	
+	@Test
+	void checkAnInvalidISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.validateISBN(2355201);
+		assertFalse(result);
 	}
 
 }
