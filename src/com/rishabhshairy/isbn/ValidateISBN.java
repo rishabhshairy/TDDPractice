64dedc07c0b88175663f555fe9b5251245cfc8cc
Rishabh Shairy <rishusinha8@gmail.com>
2021-12-14 14:20:24 +0530

    Some random logic to check first the business
    Going from Red to Green phase

--- src/com/rishabhshairy/isbn/ValidateISBN.java
+++ src/com/rishabhshairy/isbn/ValidateISBN.java
@@ -2,4 +2,12 @@ package com.rishabhshairy.isbn;
 
 public class ValidateISBN {
 
+	public boolean validateISBN(int isbn) {
+		// TODO Auto-generated method stub
+		if (isbn == 9355201) {
+			return true;
+		}
+		return false;
+	}
+
 }
